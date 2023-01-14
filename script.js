34405718bfc5994289a55f5579944104ac5abc92
Marc Carlson <55961315+MarcRichardCarlson@users.noreply.github.com>
2023-01-14 11:08:57 +0100

    Update

--- script.js
+++ script.js
@@ -28,46 +28,33 @@ function getData() {
 
 getData();
 
-/*function getData() {
-    fetch("https://fnd22-shared.azurewebsites.net/api/Cases")
-        .then((res) => res.json())
-        .then((data) => {
-            const outputEl = document.getElementById("output");
-            for (const item of data) {
-                const div = document.createElement("div");
-                div.innerHTML = item.id;
-                outputEl.appendChild(div);
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
-getData();*/
-
-
 /*STANDARD POST REQUEST USING FETCH*/
 document.getElementById("addPost").addEventListener("submit", addPost);
 
 function addPost(e) {
     e.preventDefault();
 
-    let title = document.getElementById("title").value;
-    let body = document.getElementById("body").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
-    let subject = document.getElementById("subject").value;
+    const title = document.getElementById("title").value;
+    const body = document.getElementById("body").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    const subject = document.getElementById("subject").value;
 
+    const data = {
+        title,
+        body,
+        email,
+        message,
+        subject,
+    };
+    
     fetch("https://fnd22-shared.azurewebsites.net/api/Cases", {
         method: "POST",
-        email: 'Test@gmail.com',
-        message: 'Fix this stuff',
-        subject: 'Case',
         headers: {
             Accept: "application/json, text/plain, *//*",
             "Content-type": "application/json",
         },
-        body: JSON.stringify({ title: title, body: body, email:email, message:message, subject:subject}),
+        body: JSON.stringify(data),
     })
         .then((res) => res.json())
         .then((data) => console.log(data))
@@ -76,27 +63,3 @@ function addPost(e) {
         });
 }
 
-/*
-function addPost(){
-  fetch('https://fnd22-shared.azurewebsites.net/api/Cases')
-    .then((res) => res.json())
-    .then((data) => {
-      let output = '<h2>Posts</h2>'
-      data.foreach(function(post){
-        output += `
-        <div>
-          <h3>${post.paths}</h3>
-          <p>${post.components}</p>
-          <p>${post.info}</p>
-        </div>
-        `
-      });
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-
-addPost()
-*/
