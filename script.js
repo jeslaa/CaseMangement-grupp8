a5ae5d1cec73eef76968d9a63e9deca27b6e232c
jeslaa <jesperlaaksonen@hotmail.com>
2023-01-17 23:05:56 +0100

    laddar upp försök på put metod

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ function getData() {
                     <div class="api-data2">COMMENT: &nbsp; ${content.message}</div>
                     </div>
                     <button class="edit-btn" onclick="configureEditButton;">Edit</button>
+                    <button id="status-btn" class="status-btnClass" onclick="changeStatus()">Change status</button> 
                 </div>`;
+                
                 })
                 .join("");
         })
@@ -27,24 +29,6 @@ function getData() {
 }
 
 getData();
-
-/*function getData() {
-    fetch("https://fnd22-shared.azurewebsites.net/api/Cases")
-        .then((res) => res.json())
-        .then((data) => {
-            const outputEl = document.getElementById("output");
-            for (const item of data) {
-                const div = document.createElement("div");
-                div.innerHTML = item.id;
-                outputEl.appendChild(div);
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
-getData();*/
-
 
 /*STANDARD POST REQUEST USING FETCH*/
 document.getElementById("addPost").addEventListener("submit", addPost);
@@ -76,30 +60,102 @@ function addPost(e) {
         });
 }
 
-/*
-function addPost(){
-  fetch('https://fnd22-shared.azurewebsites.net/api/Cases')
-    .then((res) => res.json())
-    .then((data) => {
-      let output = '<h2>Posts</h2>'
-      data.foreach(function(post){
-        output += `
-        <div>
-          <h3>${post.paths}</h3>
-          <p>${post.components}</p>
-          <p>${post.info}</p>
-        </div>
-        `
-      });
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+
+
+function changeStatus(){
+        fetch("https://fnd22-shared.azurewebsites.net/api/Cases/c747cdee-336a-4c8e-b38b-06fdcbbe1d55", {
+            method: "PUT",
+            status: {
+                id: 1,
+                statusName: 'Ej påbörjad',
+            },
+            headers: {
+                Accept:"application/json, text/plain, *//*",
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({status: {
+                id: 2, 
+                statusName: "Pågående"
+            }
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => {
+                console.log(err);
+            });
+    
+    getData()
 }
 
-addPost()
-*/
+
+
+
+
+
+
+
+// const statusBtn = document.querySelector('.status-btnClass') //statusBtn är null atm
+// console.log(statusBtn);
+
+
+// statusBtn.addEventListener('click', function() {
+//     function statusPut() {
+//         console.log('click');
+
+//         fetch("https://fnd22-shared.azurewebsites.net/api/Cases/c747cdee-336a-4c8e-b38b-06fdcbbe1d55", {
+//             method: "PUT",
+//             status: {
+//                 id: 1,
+//                 statusName: 'Ej påbörjad',
+//             },
+//             headers: {
+//                 Accept: "application/json, text/plain, *//*",
+//                 "Content-type": "application/json",
+//             },
+//             body: JSON.stringify({statusName: "Pågående"}),
+//         })
+//             .then((res) => res.json())
+//             .then((data) => console.log(data))
+//             .catch((err) => {
+//                 console.log(err);
+//             });
+//     }
+//     statusPut()
+//     getData()
+// })
+
+
+
+// function statusPut() {
+
+//     let title = document.getElementById("title").value;
+//     let body = document.getElementById("body").value;
+//     let email = document.getElementById("email").value;
+//     let message = document.getElementById("message").value;
+//     let subject = document.getElementById("subject").value;
+
+//     fetch("https://fnd22-shared.azurewebsites.net/api/Cases/c747cdee-336a-4c8e-b38b-06fdcbbe1d55", {
+//         method: "PUT",
+//         email: 'Test@gmail.com',
+//         message: 'Fix this stuff',
+//         subject: 'Case',
+//         headers: {
+//             Accept: "application/json, text/plain, *//*",
+//             "Content-type": "application/json",
+//         },
+//         body: JSON.stringify({ title: title, body: body, email:email, message:message, subject:subject, status: "Pågående"}),
+//     })
+//         .then((res) => res.json())
+//         .then((data) => console.log(data))
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// }
+// statusPut()
+
+
+
 
 
 
