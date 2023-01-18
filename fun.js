1c22a03e07afdb608de7916c9d4cf9dcd4f10eef
Marc Carlson <MarcRichardCarlson@users.noreply.github.com>
2023-01-18 19:34:34 +0100

    Uppdatering3

---
+++ fun.js
@@ -0,0 +1,45 @@
+
+//DARK MODE
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+    localStorage.setItem("darkMode", "enabled");
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+    localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+
+    if (darkMode !== "enabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
+
+//LIVE CLOCK
+const span = document.getElementById("span");
+
+function time() {
+    let d = new Date();
+    let s = d.getSeconds();
+    let m = d.getMinutes();
+    let h = d.getHours();
+    span.textContent =
+        ("0" + h).substr(-2) +
+        ":" +
+        ("0" + m).substr(-2) +
+        ":" +
+        ("0" + s).substr(-2);
+}
+
+setInterval(time, 1000);
